deee156c48accdf69619274a42e960530af6ff21
kevton972 <kevin.clerima@outlook.fr>
2023-06-02 21:36:52 +0200

    improve js file

--- src/clone_chatgpt/static/js/index.js
+++ src/clone_chatgpt/static/js/index.js
@@ -1,19 +1,19 @@
 import key from './creds.js'
 
 function submitFunction() {
-    const userInput = document.getElementById('user-input')
+    const userInput = document.getElementById('user-input').value
     const API_KEY = key()
 
     $.ajax({
-        url: "https://api.openai.com/v1/completions",
+        url: "https://api.openai.com/v1/chat/completions",
         type: "POST",
         header: {
-            "Autorization": "Bearer "+API_KEY,
+            "Autorization": "Bearer " + API_KEY,
             "Content-Type": 'application/json',
         },
         data: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userInput.value }],
+            messages: [{role: "user", content: userInput}],
             max_tokens: 100,
         }),
         dataType: "json",
@@ -24,4 +24,7 @@ function submitFunction() {
         error: function(error) {
           console.error("Erreur :", error);
         }})
-}
+};
+
+const btn = document.querySelector('#submit')
+btn.addEventListener('click', submitFunction)
