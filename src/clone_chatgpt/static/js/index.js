c1db7895285f21cd0bc371b436e911f8f7694e43
kevton972 <kevin.clerima@outlook.fr>
2023-06-15 22:08:08 +0200

    added functions in javascript file

--- src/clone_chatgpt/static/js/index.js
+++ src/clone_chatgpt/static/js/index.js
@@ -1,6 +1,7 @@
 import key from './creds.js'
 
 const btn = document.querySelector('#submit')
+const bottomDiv = document.querySelector('.bottom-page')
 let userInput = document.querySelector('#user-input')
 
 function getCookie(name) {
@@ -18,6 +19,38 @@ function getCookie(name) {
   }
   return cookieValue;
 }
+
+
+async function sendUserInput(){
+  try {
+    const response = await $.ajax({
+      url: "get_and_return_user_input/",
+      type: "POST",
+      headers: {
+        'X-CSRFToken': csrftoken,
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({
+        response: userInput.value
+      }),
+      dataType: "json",
+      success: function(response) {
+        const user_response = response.user_input;
+        clearExampleContainer()
+        clearChatHeader()
+        displayUserInput(user_response)
+
+        const historyPElement = document.createElement("p");
+        historyPElement.innerText = userInput.value;
+        document.querySelector('.history').appendChild(historyPElement);
+        console.log('Réponse envoyée au backend avec succès');
+      },
+    });
+  }
+    catch (error) {
+      console.error("Erreur :", error);
+    }
+};
 
 
 const csrftoken = getCookie('csrftoken');
@@ -66,10 +99,9 @@ async function sendReplyToMyView(){
       }),
       dataType: "json",
       success: function(response) {
-        const pElement = document.createElement("p");
         const chatResponse = response.chatgpt_response;
-        pElement.innerText = chatResponse
-        document.getElementById('chat-messages').appendChild(pElement)
+
+        displayChatResponse(chatResponse)
         console.log('Réponse envoyée au backend avec succès');
       },
       error: function(error) {
@@ -77,35 +109,6 @@ async function sendReplyToMyView(){
       }
     });
   }
-
-
-async function sendUserInput(){
-    try {
-      const response = await $.ajax({
-        url: "get_and_return_user_input/",
-        type: "POST",
-        headers: {
-          'X-CSRFToken': csrftoken,
-          "Content-Type": "application/json",
-        },
-        data: JSON.stringify({
-          response: userInput.value
-        }),
-        dataType: "json",
-        success: function(response) {
-          clearExampleContainer()
-          const pElement = document.createElement("p");
-          const user_response = response.user_input;
-          pElement.innerText = user_response
-          document.getElementById('user-chat').appendChild(pElement)
-          console.log('Réponse envoyée au backend avec succès');
-        },
-      });
-    }
-      catch (error) {
-        console.error("Erreur :", error);
-      }
-  };
 
 
 async function clearInput(){
@@ -117,6 +120,90 @@ async function clearExampleContainer(){
     const exampleContainer = document.querySelector('.example-container')
     exampleContainer.style.display='none'
 }
+
+
+async function clearChatHeader(){
+    const chatHeader = document.querySelector('#chat-header')
+    chatHeader.style.display='none'
+}
+
+
+async function displayUserInput(response){
+    const chat = document.getElementById("chat");
+      if (chat){
+          createUserChatDiv(response, chat)
+      } else {
+          createChatAndUserChatDiv(response)
+      }
+}
+
+
+async function createUserChatDiv(response, divWhithChatId){
+    const divChat = divWhithChatId;
+    const divUserChat = document.createElement("div");
+    const pElement = document.createElement("p");
+    const userInput = response;
+    pElement.innerText = userInput;
+    divUserChat.id = 'user-chat';
+
+    divUserChat.appendChild(pElement);
+    divChat.appendChild(divUserChat);
+}
+
+
+async function createChatAndUserChatDiv(response){
+    const chatContainer = document.querySelector('#chat-container');
+    const divChat = document.createElement("div");
+    const userChat = document.createElement("div");
+    const pElement = document.createElement("p");
+    const userInput = response;
+    pElement.innerText = userInput;
+    divChat.id = 'chat';
+    userChat.id = 'user-chat';
+
+    userChat.appendChild(pElement);
+    divChat.appendChild(userChat);
+    chatContainer.prepend(divChat);
+}
+
+
+async function displayChatResponse(response){
+    const chat = document.getElementById("chat");
+    if (chat){
+      console.log('il y a')
+        createChatMessageDiv(response, chat)
+    }
+}
+
+
+async function createChatMessageDiv(response, chatElement){
+    const divChat = chatElement;
+    const divChatMessages = document.createElement("div");
+    const pElement = document.createElement("p");
+    const chatResponse = response;
+    pElement.innerText = chatResponse;
+    divChatMessages.id = 'chat-messages';
+
+    divChatMessages.appendChild(pElement);
+    divChat.appendChild(divChatMessages);
+}
+
+
+async function createChatandCHatMessagesDiv(string){
+    const chatContainer = document.querySelector('#chat-container');
+    const divChat = document.createElement("div");
+    const divChatMessages = document.createElement("div");
+    const pElement = document.createElement("p");
+    const chatResponse = string;
+    pElement.innerText = chatResponse;
+    divChat.id = 'chat';
+    divChatMessages.id = 'chat-messages';
+
+    divChatMessages.appendChild(pElement);
+    divChat.appendChild(divChatMessages);
+    chatContainer.prepend(divChat);
+}
+
 
 async function chatDisplay(){
   await sendUserInput()
